f7da497dfc1ca002861fd7a3be4080ec0887acd9
Izavan Correia <isc1@discente.ifpe.edu.br>
2021-02-18 16:38:39 -0300

    Primeiro pacote em R, faz as quatros operações matemáticas básicas

---
+++ R/calculaty.R
@@ -0,0 +1,67 @@
+#' Function somar
+#'
+#' This function is used to add two values
+#'
+#' @param a first number.
+#' @param b second number.
+#'
+#' @example
+#' somar (200, 100)
+#'
+#' Console response: 300
+#'
+#' @export
+somar <- function (a,b) {
+  a + b;
+}
+
+#' Function subtrair
+#'
+#' This function is used to subtract two values
+#'
+#' @param a first number.
+#' @param b second number.
+#'
+#' @example
+#' subtrair (200, 100)
+#'
+#' Console response: 100
+#'
+#' @export
+subtrair <- function (a,b) {
+  a - b;
+}
+
+#' Function multiplicar
+#'
+#' This function is used to multiply two values
+#'
+#' @param a first number.
+#' @param b second number.
+#'
+#' @example
+#' multiplicar (200, 100)
+#'
+#' Console response: 20000
+#'
+#' @export
+multiplicar <- function (a,b) {
+  a * b;
+}
+
+#' Function dividir
+#'
+#' This function is used to share two values
+#'
+#' @param a first number.
+#' @param b second number.
+#'
+#' @example
+#' dividir (200, 100)
+#'
+#' Console response: 2
+#'
+#' @export
+dividir <- function (a,b) {
+  a / b;
+}
